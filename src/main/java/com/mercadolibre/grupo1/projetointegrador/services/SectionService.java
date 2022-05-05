6179fbcaa0edb6431961ad8f61de0a9b29fcaf7e
Naycoca <nayara.coca@mercadolivre.com>
2022-05-05 13:43:57 -0300

    query para calcular o espaço disponível por sessão. criado endpoint de pesquisa

--- src/main/java/com/mercadolibre/grupo1/projetointegrador/services/SectionService.java
+++ src/main/java/com/mercadolibre/grupo1/projetointegrador/services/SectionService.java
@@ -1,12 +1,17 @@
 package com.mercadolibre.grupo1.projetointegrador.services;
 
 import com.mercadolibre.grupo1.projetointegrador.dtos.SectionDTO;
+import com.mercadolibre.grupo1.projetointegrador.dtos.SectionSearchDTO;
+import com.mercadolibre.grupo1.projetointegrador.entities.Product;
 import com.mercadolibre.grupo1.projetointegrador.entities.Section;
 import com.mercadolibre.grupo1.projetointegrador.exceptions.EntityNotFoundException;
+import com.mercadolibre.grupo1.projetointegrador.exceptions.NotFoundException;
 import com.mercadolibre.grupo1.projetointegrador.repositories.InboundOrderRepository;
 import com.mercadolibre.grupo1.projetointegrador.repositories.SectionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 @Service
@@ -22,5 +27,18 @@ public class SectionService {
     public Section findBySectionDto(SectionDTO sectionDto) {
         return sectionRepository.findByIdAndWarehouse_Id(sectionDto.getSectionCode(), sectionDto.getWarehouseCode())
                 .orElseThrow(() -> new EntityNotFoundException("Sessão e/ou Armazem não encontrado na base de dados"));
+    }
+
+    /**
+     * Busca a lista de sessões por armazem
+     * @author Nayara Coca
+     */
+    public List<SectionSearchDTO> findSection(Long warehousesId){
+        List<SectionSearchDTO> sectionByWarehouse = sectionRepository.findByWarehouseId(warehousesId);
+
+        if (sectionByWarehouse.isEmpty()){
+            throw new NotFoundException("ARMAZÉM NÃO ENCONTRADO");
+        }
+        return sectionByWarehouse;
     }
 }
